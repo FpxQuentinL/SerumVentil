94883c9d2e723a8a48b4fde17a0e2c8e00ab1ba2
FpxQuentinL <46895749+FpxQuentinL@users.noreply.github.com>
2019-03-04 12:59:08 +0100

    Add files via upload

---
+++ jeu.cpp
@@ -0,0 +1,222 @@
+#include "jeu.h"
+#include <iostream>
+#include <QDebug>
+#include <QCoreApplication>
+#include <QJsonObject>
+#include <audiocontroller.h>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <array>
+#include <wiringPi.h>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
+
+
+
+
+Jeu::Jeu(IPluginsReport *report) : IPlugins(report)
+{
+
+}
+
+
+
+bool Jeu::Game_Fail()
+{
+    QJsonObject play_sound
+    {
+        {"command", "stop"},
+        {"id", "file:///home/pi/Downloads/test.mp3"}
+    };
+
+    std::cout << "YOU LOOSE !!!" << std::endl;
+    Signal_RCon_AudioControler(play_sound);
+    return (true);
+}
+
+void Jeu::loadConfigGameTech(QString config_file)
+{
+    QJsonDocument docGameTech;
+    QDir dirGameTech;
+    qDebug() << dirGameTech.path();
+    QFile fileGameTech(config_file);
+    if (!fileGameTech.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+          qDebug("error FFFIILLLEEEUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+          return;
+    }
+    qDebug() << "Opening config file" << config_file;
+    docGameTech = QJsonDocument::fromJson(fileGameTech.readAll());
+    QJsonObject object = docGameTech.object();
+    qDebug()<< docGameTech;
+    qDebug("\n\n###\t start json file\t###");
+
+    _OutputRelayLed = object.value("OutputRelayLed").toString().toInt();
+    _OutputRelayFum = object.value("OutputRelayFum").toString().toInt();
+    _LedV = object.value("ControlerLedV").toString().toInt();
+    _LedR = object.value("ControlerLedR").toString().toInt();
+    _Serum = object.value("DigitSerum").toString().toInt();
+    _Ventil = object.value("Ventil").toString().toInt();
+    qDebug()<<_Ventil ;
+    qDebug("###\t end json file###\n\n");
+
+}
+void Jeu::loadConfigGameFonct(QString config_file)
+{
+    QJsonDocument docGame;
+    QDir dirGame;
+    qDebug() << dirGame.path();
+    QFile fileGame(config_file);
+    if (!fileGame.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+          qDebug("error FFFIILLLEEEUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+          return;
+    }
+    qDebug() << "Opening config file" << config_file;
+
+    docGame = QJsonDocument::fromJson(fileGame.readAll());
+    qDebug()<<docGame;
+    QJsonObject object = docGame.object();
+
+
+    qDebug("\n\n###\t start json file\t###");
+    _AudioPathVentil= object.value("AudioPathVentil").toString();
+    if (object.contains("AudioPathWaitFVentil"))
+    _AudioPath = object.value("AudioPathWaitFVentil").toString();
+
+    qDebug("###\t end json file###\n\n");
+
+}
+
+
+
+void Jeu::Setup()
+{
+    wiringPiSetupGpio();
+    pinMode(_OutputRelayFum,OUTPUT);
+    pinMode(_OutputRelayLed,OUTPUT);
+    pinMode(_LedR,OUTPUT);
+    pinMode(_LedV,OUTPUT);
+    pinMode(_Ventil,OUTPUT);
+    pinMode(_Serum,INPUT);
+    pullUpDnControl(_Serum,PUD_DOWN);
+    digitalWrite(_OutputRelayFum,LOW);
+    digitalWrite(_LedR,LOW);
+    digitalWrite(_LedV,LOW);
+    digitalWrite(_OutputRelayLed,LOW);
+    _Time = QTime::currentTime();
+    loadConfigGameFonct("/home/pi/Dev/CodeSequenceur/test_du_soir4/Code_brief/build-generik-Desktop-Debug/GameFonct.json");
+    loadConfigGameTech("/home/pi/Dev/CodeSequenceur/test_du_soir4/Code_brief/build-generik-Desktop-Debug/GameTech.json");
+    //_BlinkLedR = new Blink();
+    //connect(&_Clignotement, &QTimer::timeout, this->_BlinkLedR, &Blink::Blinker());
+    //_BlinkLedR->_Gpio = _LedR;
+}
+
+bool Jeu::Game_Socketio()
+{
+    QJsonObject packet
+    {
+        {"io_socket", "iterate_and_pulse"},
+        {"command", "{\"color\":\"0xFAFAFA\",\"delay\":\"1\",\"brightness_max\":\"255\",\"brightness_min\":\"32\",\"brightness_scale\":\"5\"pos_start\":\"0\",\"pos_end\":\"144\",\"scale\":\"i\"}"}
+    };
+
+    std::cout << "YOU Socket iO !!!" << std::endl;
+    Signal_RCon_NetworkIO(packet);
+    return (true);
+}
+
+void Jeu::Slot_RCon( QJsonObject packet )
+{
+   qDebug()<<">IPlugins::Slot_RCon<"<< "\n" << packet;
+
+        if (packet.contains("param"))
+        {
+            if (packet.value("param")=="VentilationONOFF")
+            {
+              _Ventilation = packet.value("State").toBool();
+            }
+
+        }
+}
+void Jeu::Clignotement(int Led)
+{
+    digitalWrite(Led,!digitalRead(Led));
+}
+
+
+//GameFlow
+void Jeu::main_game_callback()
+{
+        if(digitalRead(_Ventil))
+        {
+            _Ventilation = true;
+        }
+        else
+        {
+            _Ventilation = false;
+        }
+
+
+      if(_Ventilation)
+      {
+          if (_FlagPlayOnceAudioV)
+          {
+          _FlagPlayOnceAudioV = false;
+          QJsonObject AudioListenV;
+          AudioListenV.insert("command","play");
+          AudioListenV.insert("recast",true);
+          AudioListenV.insert("path",_AudioPathVentil);
+          Signal_RCon_AudioControler(AudioListenV);
+          qDebug ()<<"je lance la musique "<< AudioListenV;
+          }
+          if (digitalRead(_Serum))
+          {
+              digitalWrite(_LedV,HIGH);
+              digitalWrite(_LedR,LOW);
+              digitalWrite(_OutputRelayFum, HIGH);
+              digitalWrite(_OutputRelayLed, HIGH);
+              qDebug()<<"Serum OK";
+          }
+          else
+          {
+              digitalWrite(_LedR,HIGH);
+              digitalWrite(_LedV,LOW);
+          }
+      }
+      else
+      {
+         // qDebug()<<digitalRead(_testVentil)<< _Ventilation << _testVentil;
+          QJsonObject AudioStop;
+          AudioStop.insert("command","stop");
+          AudioStop.insert("force","true");
+          AudioStop.insert("id",_AudioPathVentil);
+
+          _FlagPlayOnceAudioV=true;
+          if (digitalRead(_Serum))
+          {
+              if (QTime::currentTime()>_Time)
+              {
+                  Clignotement(_LedR);
+                  _Time = QTime::currentTime().addMSecs(_BlinkT);
+              }
+
+              if((!_AudioPath.isEmpty())&&(_FlagPlayOnceAudioWFV))
+              {
+                _FlagPlayOnceAudioWFV=false;
+                QJsonObject AudioListenWFV;
+                AudioListenWFV.insert("command","play");
+                AudioListenWFV.insert("path",_AudioPath);
+                Signal_RCon_AudioControler(AudioListenWFV);
+              }
+          }
+          else
+          {
+              digitalWrite(_LedR,HIGH);
+              _FlagPlayOnceAudioWFV = true;
+          }
+      }
+
+
+}
---
+++ serialport.cpp
@@ -0,0 +1,86 @@
+#include "serialport.h"
+#include <QCoreApplication>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+
+SerialPort::SerialPort(QSerialPort *serialPort, QObject *parent) :
+    QObject(parent),
+    m_serialPort(serialPort),
+    m_standardOutput(stdout)
+{
+    connect(m_serialPort, &QSerialPort::readyRead, this, &SerialPort::handleReadyRead);
+    //connect(m_serialPort, &QSerialPort::errorOccurred, this, &SerialPortReader::handleError);
+    connect(&m_timer, &QTimer::timeout, this, &SerialPort::handleTimeout);
+    m_timer.start(2000);
+    qDebug() << "DEBUG CONECT OK";
+}
+
+void SerialPort::handleReadyRead()
+{
+    QString Data;
+    QJsonObject object;
+    qDebug() << ">>>SerialPort::handleReadyRead<<<" << this;
+  if (m_serialPort->bytesAvailable() > 3)
+  {
+    m_readData.append(m_serialPort->readAll());
+
+    //affiche les data (pas de traitement...)
+    qDebug() << m_readData;
+    //delete les data au fur et a mesure quon les consome
+
+    if (!m_timer.isActive())
+        m_timer.start(100);
+    int list[3];
+    int i =0;
+    while (m_readData[i]!=13 && i<3)
+    {
+    list[i]=m_readData[i];
+    Data +=(char)list[i];
+    i+=1;
+    }
+    }
+    object.insert("command", "CallbackDetect");
+    object.insert("DataDetect",Data.toInt());
+
+    Signal_RCon_Game(object);
+    m_readData.clear();
+
+  }
+
+void SerialPort::handleReadyWrite(char *data)
+{
+    qDebug() << ">>>SerialPort::handleReadyWrite<<<" << this;
+
+    m_serialPort->write(data);
+}
+
+
+void SerialPort::handleTimeout()
+{
+
+    qDebug() << "serial timeout" << endl;
+    //call get_all au timeout pour affiche en boucle les valeurs
+    handleReadyWrite("get_all\n");
+}
+
+void SerialPort::Slot_RCon(QJsonObject packet)
+{
+    qDebug() << "SerialPort::Slot_RCon";
+    handleReadyWrite("get_all\n");
+}
+
+void SerialPort::handleError(QSerialPort::SerialPortError serialPortError)
+{
+    if (serialPortError == QSerialPort::ReadError) {
+        m_standardOutput << QObject::tr("An I/O error occurred while reading "
+                                        "the data from port %1, error: %2")
+                            .arg(m_serialPort->portName())
+                            .arg(m_serialPort->errorString())
+                         << endl;
+
+    }
+}
+
+
